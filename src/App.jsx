b600cb72a89d990330141f15a96f81b774be6ffc
Larissa Guilherme <lguilherme@gsf.local>
2024-03-22 17:19:28 -0400

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import GoogleTagManager from './components/GoogleTagManager';
 import { Container } from 'react-bootstrap';
 import { Home } from './pages/Home';
 import { Header } from './components/Header/Header';
 import { NavBar } from './components/Header/NavBar/NavBar';
 import { Footer } from './components/Footer';
 import Partners from './pages/Partners';
+import ReactGA from 'react-ga';
 
 export const App = () => {
+  useEffect(() => {
+    ReactGA.initialize('G-L252H4BG6G');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Router>
-      <GoogleTagManager />
       <Header>
         <NavBar />
       </Header>
