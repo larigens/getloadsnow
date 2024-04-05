6eb9c790d22019b6c0a5a8e9395cd92992534ccb
Larissa Guilherme <lguilherme@gsf.local>
2024-04-05 16:26:01 -0400

    removed iframe for now

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,12 +8,13 @@ export const Home = () => {
         <>
             <HelmetCP pageTitle={'Load Board for Trucking Companies'} pageDescription={'Find and post loads for trucking companies on our free load board. Get access to thousands of loads daily.'} pageKeywords={'load board, trucking load board, trucking companies, find loads, post loads, get loads, free load board, book loads'} />
             <Container className="d-flex align-items-center justify-content-center my-4">
-                <Container fluid className="p-0">
-                    <iframe
+                <Container fluid className="p-0 d-flex align-items-center" style={{ minHeight: '75vh' }}>
+                    <h1 className='light-accent' style={{ fontSize: '60px' }}>Coming Soon!</h1>
+                    {/* <iframe
                         title="gsf load board"
                         style={{ border: 'none', width: '100%', height: '1100px' }}
                         src="https://www.directfreight.com/gsquared-home/boards/find/loads"
-                    ></iframe>
+                    ></iframe> */}
                 </Container>
             </Container>
             <Container fluid className='d-flex align-items-center justify-content-center my-3'>
