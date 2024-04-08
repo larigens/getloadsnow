b3f30908dc71edf9c9adbe40a1b95b7157e1916f
Larissa Guilherme <lguilherme@gsf.local>
2024-04-08 14:42:02 -0400

    fixed server file to support client side

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 const path = require('path');
-const express = require('express'); 
+const express = require('express');
+const { createServer } = require('http');
+
+const hostname = 'localhost';
+const port = process.env.PORT || 3001;
 
 const app = express();
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Allows for rich objects and arrays to be encoded into the URL-encoded format.
 
-// Handle any other requests by serving the 'index.html' file
+// Supports the client side 
+// Adds middleware to the Express.js app that serves static files from the client/build directory if the server is running in a production environment.
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build/')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
+
+// Route handler for the root URL path.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Start the server
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+createServer(app).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
 });
