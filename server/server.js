ab2bdc38886e215185d73adc9c2630518000fa3d
Larissa Guilherme <lguilherme@gsf.local>
2024-04-09 16:46:48 -0400

    updated server.js - now it is serving the static files correctly

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build/')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+
+console.log(`🚀 Server ready at http://localhost:${PORT}`)
