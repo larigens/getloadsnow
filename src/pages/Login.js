b9faf90a77cb83966f04b92edf2a3baa2d3f8bc5
Larissa Guilherme <lguilherme@gsf.local>
2024-05-14 16:49:29 -0400

    fixed login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,8 @@
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import '../App.css';
+import { Link } from 'react-router-dom';
 import { Banner } from '../components/Banner.js';
 import { HelmetCP } from '../components/Helmet.js';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Layout } from '../components/Layout.js';
 
 export const Login = () => {
     const [iframeLoaded, setIframeLoaded] = useState(false);
@@ -18,37 +16,42 @@ export const Login = () => {
         }
     }, []);
 
-    const source = 'https://www.directfreight.com/gsquared-home/user/login'
-
     return (
         <>
             <HelmetCP pageTitle={'Login - Load Board for Trucking Companies'} pageDescription={'Find and post loads for trucking companies on our free load board. Get access to thousands of loads daily.'} pageKeywords={'load map, interactive map, trucking load board, trucking companies, find loads, post loads, get loads, free load board, book loads'} />
-            <Container fluid className="d-flex align-items-center justify-content-center my-4">
-                {!iframeLoaded ? (
-                    <>
-                        <Container fluid className="p-2 my-4 d-flex align-items-center flex-column" style={{ minHeight: '30vh' }}>
-                            <h1 className="mb-3"> You are already logged in! </h1>
-                            <Row>
-                                <Col>
-                                    <Link to='/' className='fs-22 light-accent text-none main-brand-hv fw-bold'>Find Loads</Link>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <Link to='/logout' className='my-2 fs-22 light-accent text-none main-brand-hv fw-bold'>Log Out</Link>
-                                </Col>
-                            </Row>
-                            <Row className='mt-5'>
-                                <Col className='mt-5'>
-                                    <Banner />
-                                </Col>
-                            </Row>
-                        </Container>
-                    </>
-                ) :
-                    <Layout source={source} />
-                }
+            <Container className="d-flex align-items-center justify-content-center my-4">
+                <Container fluid className="p-0 d-flex align-items-center" style={{ minHeight: '75vh' }}>
+                    {!iframeLoaded ? (
+                        <>
+                            <Container fluid className="p-2 my-4 d-flex align-items-center flex-column" style={{ minHeight: '30vh' }}>
+                                <h1 className="mb-3"> You are already logged in! </h1>
+                                <Row>
+                                    <Col>
+                                        <Link to='/' className='fs-22 light-accent text-none main-brand-hv fw-bold'>Find Loads</Link>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Link to='/logout' className='my-2 fs-22 light-accent text-none main-brand-hv fw-bold'>Log Out</Link>
+                                    </Col>
+                                </Row>
+                                <Row className='mt-5'>
+                                    <Col className='mt-5'>
+                                        <Banner />
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </>
+                    ) : null}
+                    <iframe
+                        id="gsfIframe"
+                        title="gsf load board login"
+                        style={{ border: 'none', width: '100%', height: '1100px', display: iframeLoaded ? 'block' : 'none' }}
+                        src="https://www.directfreight.com/gsquared-home/user/login"
+                    ></iframe>
+                </Container>
             </Container>
+            <Banner />
         </>
     );
 };
