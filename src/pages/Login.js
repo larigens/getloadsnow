22e8b9b576734d4582deec7875c1e2d8481bd300
Larissa Guilherme <lguilherme@gsf.local>
2024-05-24 09:30:31 -0400

    login issue test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,42 @@
-// import { Container } from 'react-bootstrap';
+import React, { useEffect } from 'react';
 import { HelmetCP } from '../components/Helmet.js';
-import { Layout } from '../components/Layout.js';
-// import { Link } from 'react-router-dom';
 
 export const Login = () => {
-    const source = 'https://www.directfreight.com/gsquared-home/user/login'
+    useEffect(() => {
+        // Open the login window when the component mounts
+        const loginWindow = window.open(
+            'https://www.directfreight.com/gsquared-home/user/login',
+            'Login',
+            'width=600,height=400'
+        );
+
+        // If the loginWindow is null, it means the popup was blocked
+        if (!loginWindow) {
+            alert("Popup blocked! Please allow popups and try again.");
+            return;
+        }
+
+        // Set an interval to check if the window is closed
+        const timer = setInterval(() => {
+            if (loginWindow.closed) {
+                clearInterval(timer);
+                // Redirect the user or perform an action on your site
+                window.location.href = 'https://getloadsnow.com/';
+            }
+        }, 1000);
+
+        // Cleanup the interval when the component unmounts
+        return () => clearInterval(timer);
+    }, []); // Empty dependency array to run only on mount and unmount
+
     return (
         <>
-            <HelmetCP pageTitle={'Login - Load Board for Trucking Companies'} pageDescription={'Find and post loads for trucking companies on our free load board. Get access to thousands of loads daily.'} pageKeywords={'load map, interactive map, trucking load board, trucking companies, find loads, post loads, get loads, free load board, book loads'} />
-            {/* <Container className='text-center'>
-                <p className='fs-18 mt-3'>Don't have an account? </p>
-                <Link to='/signup' className='text-none fs-18 mt-3 signup-btn'> Create Free Account </Link>
-            </Container> */}
-            <Layout source={source} />
+            <HelmetCP 
+                pageTitle={'Login - Load Board for Trucking Companies'} 
+                pageDescription={'Find and post loads for trucking companies on our free load board. Get access to thousands of loads daily.'} 
+                pageKeywords={'load map, interactive map, trucking load board, trucking companies, find loads, post loads, get loads, free load board, book loads'} 
+            />
+            {/* Your login UI or message can be placed here */}
         </>
     );
 };
